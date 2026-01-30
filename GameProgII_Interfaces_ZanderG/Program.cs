241b4818a191a3f420d2b2c45a4c6418aa5e67b5
Zander Goodwin <zanderngoodwin@gmail.com>
2026-01-30 01:44:41 -0400

    Player and Enemy draw on screen now

--- GameProgII_Interfaces_ZanderG/Program.cs
+++ GameProgII_Interfaces_ZanderG/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -10,12 +11,29 @@ namespace GameProgII_Interfaces_ZanderG
     internal class Program
     {
         public static Player _player = new Player(playerPosition: new Position(0,0), playerColor: ConsoleColor.Cyan);
-        public static Enemy _enemy = new Enemy(enemyPosition: new Position(0, 5), enemyColor: ConsoleColor.DarkRed);
+        public static Enemy _enemy = new Enemy(enemyPosition: new Position(10, 0), enemyColor: ConsoleColor.DarkRed);
 
         static void Main(string[] args)
         {
             IMoveStrategy _aggressiveMove = new AggressiveMoveStrategy(_player._playerPosition);
             IMoveStrategy _passiveMove = new PassiveMoveStrategy(_player._playerPosition);
+            IMoveStrategy _randomMove = new RandomMoveStrategy();
+
+            _enemy._moveStrategy = _passiveMove;
+
+            while(true)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(_player._playerPosition._xPos, _player._playerPosition._yPos);
+                Console.ForegroundColor = _player._playerColor;
+                Console.Write("*");
+
+                Console.SetCursorPosition(_enemy._enemyPosition._xPos, _enemy._enemyPosition._yPos);
+                Console.ForegroundColor = _enemy._enemyColor;
+                Console.Write("=");
+
+                Thread.Sleep(100);
+            }
         }
     }
 }
