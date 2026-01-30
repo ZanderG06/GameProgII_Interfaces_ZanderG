4a42819b72892f4d12acb2a40f52789f9ad82cc2
Zander Goodwin <zanderngoodwin@gmail.com>
2026-01-30 00:09:31 -0400

    Started Program cs

--- GameProgII_Interfaces_ZanderG/Program.cs
+++ GameProgII_Interfaces_ZanderG/Program.cs
@@ -9,12 +9,13 @@ namespace GameProgII_Interfaces_ZanderG
 {
     internal class Program
     {
-        public Player player = new Player(playerPosition: new Position(0,0), playerColor: ConsoleColor.Cyan);
-        public Enemy enemy = new Enemy(enemyPosition: new Position(0, 5), enemyColor: ConsoleColor.DarkRed);
+        public static Player _player = new Player(playerPosition: new Position(0,0), playerColor: ConsoleColor.Cyan);
+        public static Enemy _enemy = new Enemy(enemyPosition: new Position(0, 5), enemyColor: ConsoleColor.DarkRed);
 
         static void Main(string[] args)
         {
-            
+            IMoveStrategy _aggressiveMove = new AggressiveMoveStrategy(_player._playerPosition);
+            IMoveStrategy _passiveMove = new PassiveMoveStrategy(_player._playerPosition);
         }
     }
 }
