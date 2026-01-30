10404bf92eae909136ce91254c83fd6dc14f3be5
Zander Goodwin <zanderngoodwin@gmail.com>
2026-01-29 22:28:35 -0400

    Finished Random and Passive movement

--- GameProgII_Interfaces_ZanderG/PassiveMoveStrategy.cs
+++ GameProgII_Interfaces_ZanderG/PassiveMoveStrategy.cs
@@ -8,9 +8,39 @@ namespace GameProgII_Interfaces_ZanderG
 {
     internal class PassiveMoveStrategy : IMoveStrategy
     {
+        Position _playerPosition;
+
+        public PassiveMoveStrategy(Position playerPosition)
+        {
+            _playerPosition = playerPosition;
+        }
+        
         public Position Move(Position position)
         {
-            throw new NotImplementedException();
+            int _xFromPlayer = position._xPos - _playerPosition._xPos;
+            int _yFromPlayer = position._yPos - _playerPosition._yPos;
+
+            if(_xFromPlayer < _yFromPlayer && _xFromPlayer > 0 && _yFromPlayer > 0)
+            {
+                if(position._xPos + 1 <= 30) position._xPos++;
+            }
+            else if (_xFromPlayer > _yFromPlayer && _xFromPlayer > 0 && _yFromPlayer > 0)
+            {
+                if (position._yPos + 1 <= 0) position._yPos++;
+            }
+            else
+            {
+                if(_xFromPlayer < _yFromPlayer)
+                {
+                    if(position._yPos - 1 >= 0) position._yPos--;
+                }
+                else
+                {
+                    if(position._xPos - 1 >= 0) position._xPos--;
+                }
+            }
+            
+            return position;
         }
     }
 }
--- GameProgII_Interfaces_ZanderG/RandomMoveStrategy.cs
+++ GameProgII_Interfaces_ZanderG/RandomMoveStrategy.cs
@@ -8,9 +8,40 @@ namespace GameProgII_Interfaces_ZanderG
 {
     internal class RandomMoveStrategy : IMoveStrategy
     {
+        Random _random = new Random();
+        
         public Position Move(Position position)
         {
-            throw new NotImplementedException();
+            int direction = _random.Next(1,5);
+            int _xDirection = 0;
+            int _yDirection = 0;
+
+            if(direction == 1)
+            {
+                _xDirection = position._xPos + 1;
+
+                if(_xDirection <= 30) position._xPos++;
+            }
+            else if(direction == 2)
+            {
+                _xDirection = position._xPos - 1;
+
+                if(_xDirection >= 0) position._xPos--;
+            }
+            else if(direction == 3)
+            {
+                _yDirection = position._yPos + 1;
+
+                if(_yDirection <= 0) position._yPos++;
+            }
+            else
+            {
+                _yDirection = position._yPos - 1;
+
+                if(_yDirection >= 0) position._yPos--;
+            }
+
+            return position;
         }
     }
 }
